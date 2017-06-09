9ae65872010effe544e17ddb8f43bddade511742
Maxim Kovrov <maksim.kovrov@gmail.com>
2017-06-09 21:59:51 +0300

    added API key

---
+++ search/search_test.go
@@ -0,0 +1,12 @@
+package search
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestXYZ(t *testing.T) {
+	assert.True(t, false)
+	const googleKey = "AIzaSyBZeYbJ6pMNUy-VdLVxnhBCwYcWxSrZZAE"
+}
